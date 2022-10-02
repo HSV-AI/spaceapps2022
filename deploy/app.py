425f016e24784fe41229a5bc7b0c9c0ea76ce1a1
Ben <benjamin.etheredge@gmail.com>
2022-10-02 18:21:05 +0000

    Cleaning up app

--- deploy/app.py
+++ deploy/app.py
@@ -50,41 +50,46 @@ def semantic_search(text, k):
 
 
 examples = [
-    ["third rock from the sun", 1],
+    ["Zeus", 1],
     ["god of war", 2],
     ["mars gravity", 1],
-    ["Zeus", 1],
+    ["moments of inertia", 1],
     ["The Moon is a barren, rocky world without air or water. It has dark, mysterious craters and only one-eighth the gravity of Earth. It is the only place beyond Earth where humans have set foot. The Moon has been the goal of human exploration.", 1],
 ]
 
 with gr.Blocks() as demo:
     gr.Markdown("Searching data from NASA STI Program")
-    with gr.Tab("Subset of PDFs (500)"):
-        gr.Markdown("The slow API makes it hard to get more PDFs")
-        with gr.Row():
-            with gr.Column():
-                input = [gr.Textbox(label="Query", default="third rock from the sun"),
-                        gr.Slider(1, 10, 1, step=1, label='Number of results', default=1)]
-            with gr.Column():
-                output = [gr.outputs.File(label="PDFs"), gr.outputs.Textbox(label="Scores"), gr.outputs.Textbox(label="Raw Text")]
-        pdf_button = gr.Button("Search")
-
-        pdf_button.click(semantic_search, input, output)
-        gr.Examples(examples=examples, inputs=input)
 
     with gr.Tab("Titles and abstracts of all NASA STI Program"):
+        gr.Markdown("This search compares against the titles and abstracts of the NASA STI Repository.")
+        gr.Markdown("This makes use of the easily available meta data for STI documents (~500k).")
         with gr.Row():
             with gr.Column():
                 input = [gr.Textbox(label="Query", default="third rock from the sun"),
                         gr.Slider(1, 10, 1, step=1, label='Number of results', default=1)]
+                abs_button = gr.Button("Search")
             with gr.Column():
                 # output = [gr.outputs.File(label="PDFs"), gr.outputs.Textbox(label="Scores"), gr.outputs.Textbox(label="Raw Text")]
                 output = [gr.outputs.Textbox(label="Titles"), gr.outputs.Textbox(label="Scores"), gr.outputs.Textbox(label="Abstracts")]
-        abs_button = gr.Button("Search")
 
         abs_button.click(semantic_search_abstracts, input, output)
         gr.Examples(examples=examples, inputs=input)
 
+
+    with gr.Tab("Subset of PDFs"):
+        gr.Markdown("This search looks at the text in PDFs of the STI database.")
+        gr.Markdown("The slow API makes it hard to get more PDFs. We were only able to scrape ~500 PDFs.")
+        gr.Markdown("If it errors, that means something went wrong with the PDF download.")
+        with gr.Row():
+            with gr.Column():
+                input = [gr.Textbox(label="Query", default="third rock from the sun"),
+                        gr.Slider(1, 10, 1, step=1, label='Number of results', default=1)]
+                pdf_button = gr.Button("Search")
+            with gr.Column():
+                output = [gr.outputs.File(label="PDFs"), gr.outputs.Textbox(label="Scores"), gr.outputs.Textbox(label="Raw Text")]
+
+        pdf_button.click(semantic_search, input, output)
+        gr.Examples(examples=examples, inputs=input)
 
 # demo = gr.Interface(
 #     fn=semantic_search, 
